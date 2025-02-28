1d129d2e1abf9f102275bc0355c995b1705c8a74
Alec Veal <aveal@uchicago.edu>
2025-02-27 18:48:28 -0600

    Partially implement image_to_route and crop functions

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def validate_image(image_path):
     # Check image channels?
     pass
 
-def process_image(image_path, width=500):
+def process_image(image_path, width=10):
     ''' Convert image to grayscale, resize, and return edges
         Depending on certain image qualities examined in validate_image, we may want to pass different width args depending on image size and details
     '''
@@ -38,9 +38,9 @@ def process_image(image_path, width=500):
     
     # Use binary threshold. Might want to make thresholds arguments.
     thresh = 128
-    maxval = 255 
-    print(f'Applying binary threshold with threshold value {thresh} and max value {maxval}')
-    _, binary = cv2.threshold(image_gray, thresh, maxval, cv2.THRESH_BINARY)
+    max_val = 255 
+    print(f'Applying binary threshold with threshold value {thresh} and max value {max_val}')
+    _, binary = cv2.threshold(image_gray, thresh, max_val, cv2.THRESH_BINARY)
 
     # Use Canny edge detection. Might also want Canny parameters to be arguments
     lower_bound = 100 # Gradients below this threshold are non-edges
@@ -49,11 +49,13 @@ def process_image(image_path, width=500):
     edges = cv2.Canny(binary, lower_bound, upper_bound)
 
     print(type(edges))
+    print(edges)
+    # Each pixel of the edge in my ndarrray is 255. Might consider normalizing these to 1 if needed for later operations
 
     # Preview edges
-    cv2.imshow('Canny Edges', edges)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows
+    # cv2.imshow('Canny Edges', edges)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return edges
 
@@ -72,8 +74,49 @@ def plot_chicago_graph():
     CHICAGO_GRAPH_NAME = 'chicago_walk_graph.pkl.gz'
     with gzip.open(CHICAGO_GRAPH_NAME, 'rb') as f:
         graph = pickle.load(f)
-    
-    ox.plot_graph(ox.project_graph(graph))
+
+    return graph
+    # ox.plot_graph(ox.project_graph(graph))
+
+def crop_chicago_graph(chicago_graph, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
+    # Bounds is a tuple of north, south, east, west
+    north, south, east, west = bounds
+    bounding_box = (west, south, east, north)
+    return ox.truncate.truncate_graph_bbox(chicago_graph, bounding_box)
+
+def image_to_route(edges, chicago_graph, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
+    """
+    Map array edges to a running route
+    """
+    # Get coordinate bounds from chicago_graph. Might need this to be dynamic later.
+    # Need to convert graph to GeoDataFrames
+
+
+    width, height = edges.shape
+    north, south, east, west = bounds
+
+    # Treat each ndarray element as a coordinate normalized to the height and width of the image
+    route_nodes = []
+    for y in range(height):
+        for x in range(width):
+            if edges[y, x] == 255:
+                # Calculate latitude and longitude for each point
+                latitude = north - (y / height) * (north - south)
+                longitude = east - (x / width) * (east - west)
+                # https://stackoverflow.com/questions/69392846/why-does-new-osmnx-nearest-nodes-function-return-different-results-than-old-fu
+                nearest_node = ox.nearest_nodes(chicago_graph, longitude, latitude)
+                route_nodes.append(nearest_node)
+    # Remove duplicate nodes, but maintain order
+    # https://stackoverflow.com/questions/480214/how-do-i-remove-duplicates-from-a-list-while-preserving-order
+    route_nodes = list(dict.fromkeys(route_nodes))
+    print(route_nodes)
+    return route_nodes
+
+def generate_gpx(filename="route.gpx"):
+    pass
 
 if __name__ == '__main__': 
-    process_image('images/star.png')
+    edges = process_image('images/star.png')
+    chicago_graph = plot_chicago_graph()
+    cropped_graph = crop_chicago_graph(chicago_graph)
+    image_to_route(edges, cropped_graph)
