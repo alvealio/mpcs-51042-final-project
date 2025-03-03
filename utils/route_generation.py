4335fea93dd8a694c0f45d835ee1a763df67bf9c
Alec Veal <aveal@uchicago.edu>
2025-03-02 20:35:12 -0600

    Add docstrings and improve variable naming

--- utils/route_generation.py
+++ utils/route_generation.py
@@ -8,6 +8,16 @@ import gpxpy.gpx
 
 
 def get_map_graph(place="Chicago, Illinois, USA", filename='chicago_walk_graph.pkl.gz'):
+    """
+    Retrieves a map graph of Chicago (default) with OSMnx. Stores this graph in a compressed pickle file.
+
+    Args:
+        place (str, optional): Location for map graph retrieval.
+        filename (str, optional): Filename of graph stored.
+    
+    Returns:
+        networkx.MultiDiGraph: A directed graph of walkable streets/paths in specified location.
+    """
     if os.path.exists(filename):
         with gzip.open(filename, 'rb') as f:
             graph = pickle.load(f)
@@ -22,50 +32,60 @@ def get_map_graph(place="Chicago, Illinois, USA", filename='chicago_walk_graph.p
     
     return graph
 
-#def crop_chicago_graph(chicago_graph, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
-def crop_chicago_graph(chicago_graph, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
-    # Bounds is a tuple of north, south, east, west
+def crop_map_graph(graph, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
+    """
+    Crops map graph to a specified bounding box.
+
+    Args:
+        graph (networkx.MultiDiGraph): A directed graph of walkable streets/paths in specified location.
+        bounds (tuple, optional): A tuple (north, south, east, west) defining the crop boundaries.
+
+    Returns:
+        networkx.MultiDiGraph: Cropped map graph
+    """
     north, south, east, west = bounds
     bounding_box = (west, south, east, north)
-    return ox.truncate.truncate_graph_bbox(chicago_graph, bounding_box)
 
-def image_to_route(contours, chicago_graph, dimensions, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
+    return ox.truncate.truncate_graph_bbox(graph, bounding_box)
+
+def contour_to_route(contour, graph, dimensions, bounds=(41.900500, 41.830983, -87.617354, -87.659620)):
     """
-    Map contours to a running route
+    Maps contour points to map graph to generate a running route. Normalizes contour points to geographic 
+    coordinates then locates the nearest nodes in the map graph to construct the route.
+
+    Args:
+        contour (list of tuples): A list of (x, y) coordinates corresponding to contours of an image.
+        graph (networkx.MultiDiGraph): A directed graph of walkable streets/paths in specified location.
+        dimensions (int, int): The dimensions (width, height) of the processed image.
+        bounds (tuple of floats): A tuple (north, south, east, west) defining the contour mapping area.
+
+    Returns:
+        list: A list of map graph node IDs
     """
-    # I could improve the mapping by running this multiple times from different starting points and picking the graph with lowest error
-    # Store sum of errors (distances between node and points)
-    
     width, height = dimensions
     north, south, east, west = bounds
     bound_width = east - west
     bound_height = north - south
 
-    # Make a scale factor so image isn't stretched 
+    # Compute a scale factor to preserve aspect ratio
     scale_factor = min(bound_width / width, bound_height / height)
-
-    # Determine offsets
     horizontal_offset = (bound_width - width * scale_factor) / 2
     vertical_offset = (bound_height - height * scale_factor) / 2
     
-    # Treat each ndarray element as a coordinate normalized to the height and width of the image
     route_nodes = []
-    for (x, y) in contours:
-        # Calculate latitude and longitude for each point
+    for (x, y) in contour:
         latitude = north - (vertical_offset + y * scale_factor)
         longitude = west + (horizontal_offset + x * scale_factor)
-        # https://stackoverflow.com/questions/69392846/why-does-new-osmnx-nearest-nodes-function-return-different-results-than-old-fu
-        nearest_node = ox.nearest_nodes(chicago_graph, longitude, latitude)
+        nearest_node = ox.nearest_nodes(graph, longitude, latitude)
         route_nodes.append(nearest_node)
-    # Remove duplicate nodes, but maintain order
-    # https://stackoverflow.com/questions/480214/how-do-i-remove-duplicates-from-a-list-while-preserving-order
+    # Remove duplicate nodes while preserving order
     route_nodes = list(dict.fromkeys(route_nodes))
+
     # Connect each node with a series of closest nodes
     complete_route = []
-
     for i in range(len(route_nodes) - 1):
         try:
-            path = nx.shortest_path(chicago_graph, route_nodes[i], route_nodes[i + 1], weight="length")
+            path = nx.shortest_path(graph, route_nodes[i], route_nodes[i + 1], weight="length")
             # Prevent node duplication
             if i > 0:
                 path = path[1:]
@@ -75,24 +95,29 @@ def image_to_route(contours, chicago_graph, dimensions, bounds=(41.900500, 41.83
     # Make first node the last node to close the route
     if complete_route and complete_route[0] != complete_route[-1]:
         complete_route.append(complete_route[0])
+
     return complete_route
 
-def generate_gpx(complete_route, chicago_graph, filename="route.gpx"):
-    """ Generate gpx file from list of graph nodes"""
-    # Create instance of GPX object
+def generate_gpx(route, graph, filename="route.gpx"):
+    """
+    Generate a GPX file from list of graph nodes in a map graph.
+    
+    Args:
+        route (list): A list of map graph node IDs
+        graph (networkx.MultiDiGraph): A directed graph of walkable streets/paths in specified location.
+        filename (str, optional): Filename of the GPX file.
+
+    Returns:
+        str: Filename of the GPX file.
+    """
     gpx = gpxpy.gpx.GPX()
-    # Create instance of GPXTrack (path)
     gpx_track = gpxpy.gpx.GPXTrack()
-    # Append instance to tracks in GPX instance
     gpx.tracks.append(gpx_track)
-    # Create instance of GPXTrackSegment
     gpx_segment = gpxpy.gpx.GPXTrackSegment()
-    # Append to gpx_track 
     gpx_track.segments.append(gpx_segment)
 
-    # Append each node from the chicago graph needed for our route to the segnebt
-    for node in complete_route:
-        point = chicago_graph.nodes[node]
+    for node in route:
+        point = graph.nodes[node]
         gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(point['y'], point['x']))
     
     with open(filename, 'w') as f:
