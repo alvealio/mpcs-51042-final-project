bcfc0e4c4793912cafa60cb58753f4f103d045ec
Alec Veal <aveal@uchicago.edu>
2025-03-02 18:06:00 -0600

    Fix typo in resizing

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from utils.route_generation import *
 
 if __name__ == '__main__': 
     # get_chicago_graph()
-    image_path = 'images/star.png'
+    image_path = 'images/pikachu.png'
     if validate_image(image_path):
         contours, dims = extract_min_contours(image_path)
         contour = process_contours(contours)
-        chicago_graph = plot_chicago_graph()
+        chicago_graph = get_map_graph()
         cropped_graph = crop_chicago_graph(chicago_graph)
         full_route = image_to_route(contour, cropped_graph, dims)
         generate_gpx(full_route, cropped_graph)
